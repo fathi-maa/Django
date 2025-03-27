c0b61c4d066d4288e607862b6d509b4fe4b4ec7a
FathimaAM <amihtaf202@gmail.com>
2025-03-27 21:24:12 +0530

    scnd

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render , HttpResponse
-
+from .models import TodoItem
 # Create your views here.
 def home(request):
     return render(request , "home.html")
+def todo(request):
+    items= TodoItem.objects.all()
+    return render(request , "todo.html" ,{"todo": items} )
 
